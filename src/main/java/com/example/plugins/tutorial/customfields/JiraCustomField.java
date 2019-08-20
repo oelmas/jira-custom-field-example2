84ff615115064437532ae015e69282a80e018df2
oner elmas <oelmas@gmail.com>
2019-08-20 17:40:28 +0300

    Initial commit

---
+++ src/main/java/com/example/plugins/tutorial/customfields/JiraCustomField.java
@@ -0,0 +1,19 @@
+package com.example.plugins.tutorial.customfields;
+
+import com.atlassian.jira.issue.customfields.impl.GenericTextCFType;
+import com.atlassian.jira.issue.customfields.manager.GenericConfigManager;
+import com.atlassian.jira.issue.customfields.persistence.CustomFieldValuePersister;
+import com.atlassian.jira.issue.fields.TextFieldCharacterLengthValidator;
+import com.atlassian.jira.security.JiraAuthenticationContext;
+import com.atlassian.plugin.spring.scanner.annotation.imports.JiraImport;
+
+
+public class JiraCustomField extends GenericTextCFType {
+    public JiraCustomField(
+            @JiraImport CustomFieldValuePersister customFieldValuePersister,
+            @JiraImport GenericConfigManager genericConfigManager,
+            @JiraImport TextFieldCharacterLengthValidator textFieldCharacterLengthValidator,
+            @JiraImport JiraAuthenticationContext jiraAuthenticationContext) {
+        super(customFieldValuePersister, genericConfigManager, textFieldCharacterLengthValidator, jiraAuthenticationContext);
+    }
+}
